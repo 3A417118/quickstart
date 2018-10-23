243fdcd12f3f38a8fbfb781b2eea9c68b9ab8190
3A417118 <s3A417118@student.ncut.edu.tw>
2018-10-23 19:44:36 +0800

    fifth step addTask

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,15 +1,19 @@
 @include('common.errors')
-
 Route::post('/task', function (Request $request) {
+    // 驗證輸入
     $validator = Validator::make($request->all(), [
         'name' => 'required|max:255',
     ]);
-
     if ($validator->fails()) {
         return redirect('/')
             ->withInput()
             ->withErrors($validator);
     }
-
     // 建立該任務...
+	// 建立該任務...
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+    return redirect('/');
+
 });
