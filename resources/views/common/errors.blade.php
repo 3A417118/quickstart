6754c0b0120be7e0281b5eb5efbe062c3028a10e
3A417118 <s3A417118@student.ncut.edu.tw>
2018-10-23 19:52:04 +0800

    sixth step errorblade

--- resources/views/common/errors.blade.php
+++ resources/views/common/errors.blade.php
@@ -1,19 +1,13 @@
 @include('common.errors')
-Route::post('/task', function (Request $request) {
-    // 驗證輸入
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    // 建立該任務...
-	// 建立該任務...
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
-    return redirect('/');
-
-});
+@if (count($errors) > 0)
+    <!-- 表單錯誤清單 -->
+    <div class="alert alert-danger">
+        <strong>哎呀！出了些問題！</strong>
+        <br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
